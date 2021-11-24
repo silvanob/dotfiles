53be8e35eeb70d05d0d4171c3cc65d0449a2f47d
Silvano <silvanobiemans@gmail.com>
2021-11-24 12:37:31 +0100

    add more stuff

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,5 @@ alias vinit=viminit
 alias viminit="nvim ~/.config/nvim/init.vim"
 alias zshalias="vim $ZSH_CUSTOM/aliases.zsh"
 export GPG_TTY=$(tty)
+alias i3config="nvim ~/.config/i3/config"
+alias polybarconfig="nvim ~/.config/polybar/config"
